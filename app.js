d6ebc7f07dd3d569d65189d805727501be43e257
Ilboudo Souleymane <ilboudosouleymane4@gmail.com>
2022-05-18 18:46:30 +0000

    ok

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(favicon(__dirname + "/favicon.ico")).use(bodyParser.json());
-
+console.log(process.env);
 sequelize.initDb();
 app.get("/", (req, res) => {
   res.json("Hello heroku");
